e1d9a34a58153670e8a61d64adfb2e4eae0442ba
NotA-Hater <ahaternot@gmail.com>
2025-04-19 23:49:40 -0300

    portv3

--- config.gallery.ts
+++ config.gallery.ts
@@ -5,10 +5,111 @@ const gallery: { albums: Album[] } = {
     {
       title: "Thumbnails",
       route: "me",
-      coverImageSrc:
-        "/assets/me/me-cover.png",
+      coverImageSrc: "/assets/me/me-cover.png",
       posts: [
-        
+        {
+          title: " ",
+          src: "/assets/me/1.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/2.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/3.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/4.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/5.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/6.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/7.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/8.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/9.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/10.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/11.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/12.png",
+        },
+      ],
+    },
+    {
+      title: "Clips",
+      route: "clips",
+      coverImageSrc: "/assets/me/thumb2.png",
+      posts: [
+        {
+          title: " ",
+          src: "/assets/me/1.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/2.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/3.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/4.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/5.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/6.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/7.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/8.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/9.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/10.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/11.png",
+        },
+        {
+          title: " ",
+          src: "/assets/me/12.png",
+        },
       ],
     },
   ],
